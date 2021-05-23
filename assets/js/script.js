7e50c856d76cb336787147f25fce2a53c80ab850
charlottegracia <1086120@ucn.dk>
2021-05-23 17:29:51 +0200

    yikes

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ const apiUserCredentials = {
     password: "API-key-1234#!",
 };
 
-const id = 10;
+const id = 27;
 
 getDataFromWP();
 
@@ -29,11 +29,20 @@ function getDataFromWP() {
 }
 
 function createPage() {
+    const url = window.location.href;
     console.log(`yay token: ${window.localStorage.getItem("authToken")}`);
-    juhu();
+    if (url.indexOf('index.html') > -1) {
+        drawFrontpage();
+    }   else if (url.indexOf('adopter.html') > -1) {
+        drawAdopter();
+    }   else if (url.indexOf('katteSomSoegerHjem.html') > -1) {
+        drawKatteSomSoegerHjem();
+    } else if (url.indexOf('kat.html') > -1) {
+        drawCat();
+    }
 }
 
-function juhu() {
+function drawKatteSomSoegerHjem() {
     fetch(`${apiUrl}posts?status=private&categories=${id}&per_page=50`, {
         headers: {
             'Authorization': `Bearer ${window.localStorage.getItem("authToken")}`
@@ -42,12 +51,32 @@ function juhu() {
     .then(response => response.json()) //converts response to JSON object
     .then(data => { //passing data through arrow function
         console.log(data);
-        jubi(data);
+        let text = "";
+        data.forEach(kat => {
+            text += `<a href="kat.html?${kat.acf.navn}">`;
+            text += `<h1>${kat.acf.navn}</h1>`;
+            text += `</a>`;
+        });
+        document.querySelector('main').innerHTML = text;
     })
 }
 
-function jubi(data) {
+function drawFrontpage() {
     let text = "";
-    text += `<h1>${data[0].acf.titel_pa_kategori}</h1>`;
+    text += `<h1>forside</h1>`;
     document.querySelector('main').innerHTML = text;
+}
+
+function drawAdopter() {
+    let text = "";
+    text += `<h1>adopter</h1>`;
+    document.querySelector('main').innerHTML = text;
+}
+
+function drawCat() {
+
+    let text = "";
+    text += `<h1>kattefar</h1>`;
+    document.querySelector('main').innerHTML = text;
+
 }
