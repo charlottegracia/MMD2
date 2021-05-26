78f6f28dac8b146d31c71917c6d8706a662ec648
charlottegracia <1086120@ucn.dk>
2021-05-26 19:40:51 +0200

    jubi dubi

--- assets/js/script.js
+++ assets/js/script.js
@@ -31,71 +31,89 @@ function getDataFromWP() {
 function createPage() {
     const url = window.location.href;
     console.log(`yay token: ${window.localStorage.getItem("authToken")}`);
-    if (url.indexOf('index.html') > -1) {
-        drawFrontpage();
-    }   else if (url.indexOf('adopter.html') > -1) {
-        drawAdopter();
-    }   else if (url.indexOf('katteSomSoegerHjem.html') > -1) {
-        drawKatteSomSoegerHjem();
-    } else if (url.indexOf('kat.html') > -1) {
-        drawCat();
-    } else {
-        drawFrontpage();
-    }
+    fetch(`${apiUrl}posts?status=private&categories=${id}&per_page=50`, {
+        headers: {
+            'Authorization': `Bearer ${window.localStorage.getItem("authToken")}`
+        }
+    }) //specifies the url to fetch() method with the API key
+    .then(response => response.json()) //converts response to JSON object
+    .then(data => { //passing data through arrow function
+        console.log(data);
+        if (url.indexOf('index.html') > -1) {
+            drawFrontpage(data);
+        }   else if (url.indexOf('adopter.html') > -1) {
+            drawAdopter(data);
+        } else if (url.indexOf('kat.html') > -1) {
+            drawCat(data);
+        } else {
+            drawFrontpage(data);
+        }
+    })
 }
 
-function drawFrontpage() {
+function drawFrontpage(data) {
     let text = "";
-    //text += `<h1>Nordsjællands Kattehjælp</h1>`;
-    //document.querySelector('main').innerHTML = text;
-}
-
-function drawAdopter() {
-    let text = "";
-    text += `<h1>adopter</h1>`;
+    text += `
+    <h1>Nordsjællands Kattehjælp</h1>
+        <section>
+            <a href="adopter.html">
+                <h2>Mød kattene</h2>
+                <h3>Adoption / Bliv plejer</h3>
+            </a>
+            <a href="#">
+                <h2>Støt os</h2>
+                <h3>Donation / Bliv medlem</h3>
+            </a>
+            <a href="kat.html?${data[0].acf.navn}"><img src="${data[0].acf.billeder.billede1.url}" alt="123"></a>
+            <a href="#">
+                <h2>Kat i nød</h2>
+                <h3>Kattehjælp / FIV</h3>
+            </a>
+            <a href="kat.html?${data[1].acf.navn}"><img src="${data[1].acf.billeder.billede1.url}" alt="123"></a>
+            <a href="#">
+                <h2>Foreningen</h2>
+                <h3>Vedtægter / Generalforsamling</h3>
+            </a>
+            <a href="#">
+                <h2>Samarbejde</h2>
+                <h3>Partnere / Sponsere</h3>
+            </a>
+            <a href="#">
+                <h2>Kontakt os</h2>
+                <h3>Kontakt / GDPR</h3>
+            </a>
+        </section>
+    `;
     document.querySelector('main').innerHTML = text;
 }
 
-function drawKatteSomSoegerHjem() {
-    fetch(`${apiUrl}posts?status=private&categories=${id}&per_page=50`, {
-        headers: {
-            'Authorization': `Bearer ${window.localStorage.getItem("authToken")}`
-        }
-    }) //specifies the url to fetch() method with the API key
-    .then(response => response.json()) //converts response to JSON object
-    .then(data => { //passing data through arrow function
-        console.log(data);
-        let text = "";
-        data.forEach(kat => {
-            text += `<a href="kat.html?${kat.acf.navn}">`;
-            text += `<h1>${kat.acf.navn}</h1>`;
-            text += `</a>`;
-        });
-        document.querySelector('main').innerHTML = text;
-    })
+function drawAdopter(data) {
+    let text = "";
+    text += `
+    <h1>Mød kattene</h1>
+    <h2>Katte som søger hjem</h2>
+    `;
+    data.forEach(kat => {
+        text += `<a href="kat.html?${kat.acf.navn}">`;
+        text += `<h3>${kat.acf.navn}</h3>`;
+        text += `</a>`;
+    });
+    document.querySelector('main').innerHTML = text;
 }
 
-function drawCat() {
+function drawCat(data) {
     const url = window.location.href;
     let urlSplit = url.split('?');
-    fetch(`${apiUrl}posts?status=private&categories=${id}&per_page=50`, {
-        headers: {
-            'Authorization': `Bearer ${window.localStorage.getItem("authToken")}`
+    console.log(data);
+    data.forEach(kat => {
+        if (urlSplit[1] == kat.acf.navn) {
+            console.log("jubiii");
+            let text = "";
+            text += `
+            <h1>${kat.acf.navn}</h1>
+            <img class="katImg" src="${kat.acf.billeder.billede1.url}" alt="${kat.acf.navn}">
+            `;
+            document.querySelector('main').innerHTML = text;
         }
-    }) //specifies the url to fetch() method with the API key
-    .then(response => response.json()) //converts response to JSON object
-    .then(data => { //passing data through arrow function
-        console.log(data);
-        data.forEach(kat => {
-            if (urlSplit[1] == kat.acf.navn) {
-                console.log("jubiii");
-                let text = "";
-                text += `
-                <h1>${kat.acf.navn}</h1>
-                <img class="katImg" src="${kat.acf.billeder.billede1.url}" alt="${kat.acf.navn}">
-                `;
-                document.querySelector('main').innerHTML = text;
-            }
-         });
-    })
+    });
 }
