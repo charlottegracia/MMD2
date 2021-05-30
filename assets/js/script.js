c32b305bfeb3643d94f2e990ff51c173cd69c3d1
charlottegracia <1086120@ucn.dk>
2021-05-30 16:12:52 +0200

    mangler at lave responsiv og hente samarbejde

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,58 +39,139 @@ function createPage() {
     .then(response => response.json()) //converts response to JSON object
     .then(data => { //passing data through arrow function
         console.log(data);
+        drawNav();
         if (url.indexOf('index.html') > -1) {
             drawFrontpage(data);
         }   else if (url.indexOf('adopter.html') > -1) {
             drawAdopter(data);
         } else if (url.indexOf('kat.html') > -1) {
             drawCat(data);
+        } else if (url.indexOf('samarbejde.html') > -1) {
+            drawSamarbejdspartnere();
         } else {
             drawFrontpage(data);
         }
+        drawFooter();
     })
+}
+
+function drawNav() {
+    let text = "";
+    text += `
+        <nav>
+            <a href="index.html">
+                <img src="assets/images/NSK logo nyt.jpeg" alt="NSK Logo">
+                <h1>Nordsjællands Kattehjælp</h1>
+            </a>
+            <ul>
+                <li>
+                    <a href="adopter.html">Adopter</a>
+                </li>
+                <li>
+                    <a href="#"">Støt os</a>
+                </li>
+                <li>
+                    <a href="#">Kat i nød</a>
+                </li>
+                <li>
+                    <a href="#">Foreningen</a>
+                </li>
+                <li>
+                    <a href="samarbejde.html">Samarbejde</a>
+                </li>
+                <li>
+                    <a href="#">Kontakt</a>
+                </li>
+            </ul>
+        </nav>
+    `;
+    document.querySelector('header').innerHTML = text;
+}
+
+function drawFooter() {
+    let text = "";
+    text += `
+        <h5>info@kattehjælp.dk | Konto: reg.: 5357 konto: 0246871 | CVR nr. 123456</h5>
+        <h5>Donationer modtages på Mobilepay 12345</h5>
+    `;
+    document.querySelector('footer').innerHTML = text;
 }
 
 function drawFrontpage(data) {
     let text = "";
-    /*text += `
-    <h1>Nordsjællands Kattehjælp</h1>
+    fetch(`${apiUrl}posts?status=private&categories=${28}&per_page=50`, {
+        headers: {
+            'Authorization': `Bearer ${window.localStorage.getItem("authToken")}`
+        }
+    }) //specifies the url to fetch() method with the API key
+    .then(response => response.json()) //converts response to JSON object
+    .then(billeder => { //passing data through arrow function
+        console.log(billeder);
+        text += `
+        <h1>Nordsjællands Kattehjælp</h1>
         <section>
-            <a href="adopter.html">
-                <h2>Mød kattene</h2>
-                <h3>Adoption / Bliv plejer</h3>
-            </a>
-            <a href="#">
+            <a class="blue" href="adopter.html">
+                <h2>Adopter</h2>
+                <h3 class="darkblueText">Adoption / Bliv plejer</h3>
+            </a>`
+        if (data[0]) {
+            text += `<a href="kat.html?${data[0].acf.navn}"><img src="${data[0].acf.billeder.billede1.url}" alt="${data[0].acf.navn}"></a>`;
+        } else {
+            text += `<a><img src="${billeder[0].acf.billeder.stockbillede1.url}" alt="Kat></a>`;
+        }
+        text += `
+            <a class="darkgreen" href="#">
                 <h2>Støt os</h2>
-                <h3>Donation / Bliv medlem</h3>
+                <h3 class="greenText">Donation / Bliv medlem</h3>
             </a>
-            <a href="kat.html?${data[0].acf.navn}"><img src="${data[0].acf.billeder.billede1.url}" alt="123"></a>
-            <a href="#">
+        `;
+        if (data[1]) {
+            text += `<a href="kat.html?${data[1].acf.navn}"><img src="${data[1].acf.billeder.billede1.url}" alt="${data[1].acf.navn}"></a>`;
+        } else {
+            text += `<a><img src="${billeder[0].acf.billeder.stockbillede2.url}" alt="Kat></a>`;
+        }
+        if (data[2]) {
+            text += `<a href="kat.html?${data[2].acf.navn}"><img src="${data[2].acf.billeder.billede1.url}" alt="${data[2].acf.navn}"></a>`;
+        } else {
+            text += `
+            <a href="#"><img src="${billeder[0].acf.billeder.stockbillede3.url}" alt="Kat"></a>`;
+        }
+        text += `
+            <a class="green" href="#">
                 <h2>Kat i nød</h2>
-                <h3>Kattehjælp / FIV</h3>
+                <h3 class="darkgreenText">Kattehjælp / FIV</h3>
             </a>
-            <a href="kat.html?${data[1].acf.navn}"><img src="${data[1].acf.billeder.billede1.url}" alt="123"></a>
-            <a href="#">
+        `;
+        if (data[3]) {
+            text += `<a href="kat.html?${data[3].acf.navn}"><img src="${data[3].acf.billeder.billede1.url}" alt="${data[3].acf.navn}"></a>`;
+        } else {
+            text += `
+            <a href="#"><img src="${billeder[0].acf.billeder.stockbillede4.url}" alt="Kat"></a>`;
+        }
+        text += `
+            <a class="darkblue" href="#">
                 <h2>Foreningen</h2>
-                <h3>Vedtægter / Generalforsamling</h3>
+                <h3 class="blueText">Vedtægter / Generalforsamling</h3>
             </a>
-            <a href="#">
-                <h2>Samarbejde</h2>
-                <h3>Partnere / Sponsere</h3>
-            </a>
-            <a href="#">
-                <h2>Kontakt os</h2>
-                <h3>Kontakt / GDPR</h3>
-            </a>
-        </section>
+        </section
+        `;
+    document.querySelector('main').innerHTML = text;
+    })
+}
+
+function drawSamarbejdspartnere() {
+    console.log("HALLOOOO");
+    let text = "";
+    text += `
+    <h1>Samarbejdspartnere</h1>
     `;
-    document.querySelector('main').innerHTML = text; */
+    document.querySelector('main').innerHTML = text;
 }
 
 function drawAdopter(data) {
     let text = "";
     text += `
-    <h1>Mød kattene</h1>
+    <h1>Adopter</h1>
     <h2>Katte som søger hjem</h2>
     `;
     data.forEach(kat => {
