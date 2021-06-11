61f1dd4a3bfe026873d4020a0923c3631c7c1dcb
SimoneSoerensen <1086082@ucn.dk>
2021-06-11 10:01:27 +0200

    dumme pil
    dumme pil

--- assets/js/script.js
+++ assets/js/script.js
@@ -117,7 +117,7 @@ function drawNav() {
         </li>
     </ul>
     `;
-    document.querySelector('header').innerHTML = text;
+    document.querySelector('header').innerHTML += text;
 }
 
 function dropdownMobile() {
@@ -838,4 +838,27 @@ function dropdownKatiNoed(n) {
     } else if (n == 6) {
         document.getElementById("katINoed6").classList.toggle('hide');
     } 
+}
+
+
+
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
 }
